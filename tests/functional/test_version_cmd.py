80ca76eeca7073dbe2e4403c3c02c1987d1444d5
Xavier Chapron <xavier.chapron@ledger.fr>
2024-03-06 11:55:07 +0100

    Bump version to 0.5.3

--- tests/functional/test_version_cmd.py
+++ tests/functional/test_version_cmd.py
@@ -3,7 +3,7 @@ from apps.zilliqa import ZilliqaClient
 # Taken from the Makefile, to update every time the Makefile version is bumped
 MAJOR = 0
 MINOR = 5
-PATCH = 2
+PATCH = 3
 
 
 # In this test we check the behavior of the device when asked to provide the app version
